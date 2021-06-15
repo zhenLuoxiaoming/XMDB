ddb66d8591c75d7b54e4c261e5dd4e6bd5b6373c
罗晓明 <luoxiaoming@luoxiaomingdeMacBook-Pro.local>
2021-06-15 14:45:26 +0800

    打开数据库，本地路径修改

--- XMDB/XMDB/XMFMDB.swift
+++ XMDB/XMDB/XMFMDB.swift
@@ -55,6 +55,10 @@ open class XMFMDB  {
         
     //MARK:获取数据库路径
     public static func getDBUrl(dbname : String) -> URL {
+        if let path = Bundle.main.path(forResource:dbname, ofType:"db") {
+            let fileURL = URL.init(fileURLWithPath: path)
+            return fileURL
+        }
         let p = Bundle(for: self).resourcePath ?? ""
         if let path = Bundle(path: p)?.path(forResource:dbname, ofType:"db") {
             let fileURL = URL.init(fileURLWithPath: path)
